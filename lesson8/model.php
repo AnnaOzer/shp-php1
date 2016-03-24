716522e25c923f995170e6cfeb2f6bb39bae37af
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-24 15:46:51 +0300

    + 2 функции: реквизиты к БД и соединение с БД;

--- lesson8/model.php
+++ lesson8/model.php
@@ -1,6 +1,18 @@
 <?php
 
-$DBHost = 'localhost';
-$DBUser = 'root';
-$DBPass = '';
-$DBName = 'shp-php1-8';
+function dbCred()
+{
+    return [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => '',
+        'dbname' => 'shp-php1-8',
+    ];
+}
+
+function dbInit() {
+
+    $cred = dbCred();
+    mysql_connect($cred['host'], $cred['user'], $cred['pass']);
+    mysql_select_db($cred['dbname']);
+}
