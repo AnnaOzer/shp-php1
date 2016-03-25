404785777e8a6d66166b9bacb87fc44987bf3366
AnnaOzer <ozerskaya.ru@mail.ru>
2016-03-26 00:53:54 +0300

    + контроллер upload загружает файлы ожидаемо

--- lesson8/upload.php
+++ lesson8/upload.php
@@ -6,8 +6,7 @@ if(!empty($_FILES)) {
     if(isset($_FILES[$name])) {
         if(0 == $_FILES[$name]['error']) {
 
-            echo $newFileName = __DIR__ . '/img/' . $_FILES[$name]['name'];
-            die;
+            $newFileName = __DIR__ . '/img/' . $_FILES[$name]['name'];
 
             move_uploaded_file(
                 $_FILES[$name]['tmp_name'],
